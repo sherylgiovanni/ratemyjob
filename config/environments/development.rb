a07edcbfb5b43d81d5c4f76a39f6ebd54512fa73
Ubuntu <ubuntu@ip-172-31-19-155.us-west-2.compute.internal>
2022-10-09 22:57:28 +0000

    Change host and unblock aws

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,8 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  
+  #For AWS
+  config.hosts << "5ce6c26cda924016810887f11184ce8a.vfs.cloud9.us-west-2.amazonaws.com"
+
 end
