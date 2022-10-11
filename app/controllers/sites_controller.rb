aea501d1bd5c3bcb8f76b3a738d08b928fd66759
Ubuntu <ubuntu@ip-172-31-19-155.us-west-2.compute.internal>
2022-10-11 05:32:26 +0000

    Finish frontend

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -2,8 +2,8 @@ class SitesController < ApplicationController
   def index
   end
   
-  def about
-    flash.now[:notice] = "this is a notice"
-    flash.now[:alert] = "this is an alert"
-  end
+  # def about
+  #   flash.now[:notice] = "this is a notice"
+  #   flash.now[:alert] = "this is an alert"
+  # end
 end
