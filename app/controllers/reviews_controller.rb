d4d63df4466db35508f0c346d3a0be6580cb7fe9
Ubuntu <ubuntu@ip-172-31-19-155.us-west-2.compute.internal>
2022-10-16 22:58:13 +0000

    Add update and delete methods

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,7 @@
 class ReviewsController < ApplicationController
  before_action :load_job
+ before_action :load_review, except: [:index, :new, :create]
+ 
  def index
  end 
 
@@ -18,6 +20,23 @@ class ReviewsController < ApplicationController
  end
  
  def show
+ end
+ 
+ def edit
+ end
+ 
+ def update
+  if @review.update review_params
+      redirect_to job_review_path(@job,@review), notice: "Review has been updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+ end
+ 
+ def destroy
+   @review.destroy
+   redirect_to job_reviews_path(@job), alert: "Review has been deleted."
+ end
  
  private
  
@@ -27,5 +46,9 @@ class ReviewsController < ApplicationController
  
  def load_job
   @job = Job.find params[:job_id]
+ end
+ 
+ def load_review
+  @review = @job.reviews.find params[:id]
  end
 end
