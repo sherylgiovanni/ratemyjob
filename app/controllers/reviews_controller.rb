364ee729ece9b89919c38cced9bbaf92fec44c43
Ubuntu <ubuntu@ip-172-31-19-155.us-west-2.compute.internal>
2022-10-16 22:29:26 +0000

    Add create function

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,7 +7,23 @@ class ReviewsController < ApplicationController
   @review = Review.new
  end
  
+ def create
+  @review = Review.new review_params
+  @review.job = @job
+  if @job.reviews << @review
+   redirect_to job_review_path(@job,@review), notice: "Review added successfully."
+  else
+   render :new, status: :unprocessable_entity
+  end
+ end
+ 
+ def show
+ 
  private
+ 
+ def review_params 
+  params.require(:review).permit(:hiring_manager_name, :pros, :cons, :work_hours, :anonymous)
+ end
  
  def load_job
   @job = Job.find params[:job_id]
