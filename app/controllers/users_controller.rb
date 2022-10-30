938b406df3680ebf57894a66b5378c855bb6c52c
Ubuntu <ubuntu@ip-172-31-19-155.us-west-2.compute.internal>
2022-10-30 10:31:16 +0000

    Add create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,18 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+  
+  def create
+    @user = User.new user_params
+    if @user.save
+     redirect_to root_path, notice: "Your account has been created."
+    else
+     render :new, status: :unprocessable_entity
+    end
+  end
+  
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
 end
