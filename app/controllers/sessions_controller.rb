777c822594b1ada24bd022f433b257690459c617
Ubuntu <ubuntu@ip-172-31-19-155.us-west-2.compute.internal>
2022-10-30 10:46:34 +0000

    Enable users to login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,8 @@ class SessionsController < ApplicationController
     @user = User.where(email: params[:email]).first
     if @user && @user.authenticate(params[:password])
       #login
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: 'Welcome back!'
     else
       redirect_to login_path, alert: 'Invalid email/password.'
     end
