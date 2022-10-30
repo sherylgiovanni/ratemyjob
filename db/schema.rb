5e219a58ab60e5c5a424784f9123ab016db9a2d1
Ubuntu <ubuntu@ip-172-31-19-155.us-west-2.compute.internal>
2022-10-30 10:14:14 +0000

    Added user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_16_214211) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_30_101315) do
   create_table "jobs", force: :cascade do |t|
     t.string "job_title"
     t.text "location"
@@ -27,6 +27,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_16_214211) do
     t.text "cons"
     t.string "hiring_manager_name"
     t.boolean "anonymous"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
