18408372360ab3b9eff02dca6f47fb91578e2545
Ubuntu <ubuntu@ip-172-31-19-155.us-west-2.compute.internal>
2022-10-30 10:36:04 +0000

    Add sessions model

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   # make /signup as the standard url to go to the users#new page
   get "signup" => "users#new"
+  get "login" => "sessions#new"
   # disable users from accessing index and about if they are not logged in
   resources :users, except: [:index, :about]
+  resources :session, except: [:index, :edit, :edit, :show]
   resources :jobs do
     resources :reviews
   end
