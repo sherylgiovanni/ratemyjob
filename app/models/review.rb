f820900aac17ddcdb2140ee7eb29a505cd74d05e
Ubuntu <ubuntu@ip-172-31-19-155.us-west-2.compute.internal>
2022-10-30 11:37:13 +0000

    Establish has_many and belongs_to

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,7 @@
 class Review < ApplicationRecord
     belongs_to :job
+    belongs_to :user
+    
     validates :pros, presence: true
     validates :cons, presence: true
     validates :work_hours, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+    has_many :reviews
+    
     has_secure_password
     validates :name, presence: :true
     validates :email, format: { with: /@/, message: "Must have an @" }, uniqueness: true
