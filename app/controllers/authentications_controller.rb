e78e2205621529626669c3f57699ab4af0123d9c
Ubuntu <ubuntu@ip-172-31-19-155.us-west-2.compute.internal>
2022-11-09 06:33:22 +0000

    add new migration

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -1,14 +1,23 @@
 class AuthenticationsController < ApplicationController
-    def create
-        render json: auto_hash
+  def create
+    #render json: auto_hash
+  end
+  
+  private
+  def auth_hash
+    request.env['omniauth.auth']
+  end
+  
+  def user_hash
+    hash = {}
+    hash[:uid] = auth_hash['uid']
+    if auth_hash['info']
+      hash[:name] = auth_hash['info']['name']
+      hash[:name] = auth_hash['info']['email']
     end
-    
-    private
-    def auto_hash
-        request.env['omniauth.auth']
+    if auth_hash['credentials']
+      hash[:token] = auth_hash['credentials']['token']
     end
-    
-    def user_hash
-    
-    end
+    hash
+  end
 end
