94041e18ee5b5570bc01369fc23927238003ad14
Ubuntu <ubuntu@ip-172-31-19-155.us-west-2.compute.internal>
2022-11-29 05:50:02 +0000

    Add login test

---
+++ test/integration/sessions_test.rb
@@ -0,0 +1,29 @@
+require "test_helper"
+
+class SessionsTest < ActionDispatch::IntegrationTest
+  test "users can login" do
+    user = FactoryBot.create :user, email: "testlogin@test.com", password: "secret"
+    
+    visit root_path
+    click_on "Login"
+    fill_in "Email", with: "testlogin@test.com"
+    fill_in "Password", with: "secret"
+
+    click_button "Login"
+    
+    assert_equal page.current_path, root_path
+    
+    assert_text "Log out"
+    assert_text "Welcome"
+    refute page.has_content?("Login")
+    
+  end
+  
+  test "users can't login with wrong password" do
+    assert true
+  end
+  
+  test "users can logout" do
+    assert true
+  end
+end
