4893ae88ae963e47e5455d7111765ac7dc73a3c6
Ubuntu <ubuntu@ip-172-31-19-155.us-west-2.compute.internal>
2022-11-29 05:52:19 +0000

    Add error login test

--- test/integration/sessions_test.rb
+++ test/integration/sessions_test.rb
@@ -20,7 +20,17 @@ class SessionsTest < ActionDispatch::IntegrationTest
   end
   
   test "users can't login with wrong password" do
-    assert true
+    user = FactoryBot.create :user, email: "testlogin@test.com", password: "secret"
+    
+    visit login_path
+
+    fill_in "Email", with: "testlogin@test.com"
+    fill_in "Password", with: "Secret"
+
+    click_button "Login"
+  
+    assert_text "Invalid"
+    refute page.has_content?("Log Out")
   end
   
   test "users can logout" do
