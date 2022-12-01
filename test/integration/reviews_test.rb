d405a3da30d0078207a439f8f3571ec1b1081ae8
Ubuntu <ubuntu@ip-172-31-19-155.us-west-2.compute.internal>
2022-12-01 08:41:46 +0000

    Test users can create a job

--- test/integration/reviews_test.rb
+++ test/integration/reviews_test.rb
@@ -15,18 +15,37 @@ class ReviewsTest < ActionDispatch::IntegrationTest
     assert_text review1.pros
     assert_text review2.pros
     assert_text review3.pros
-    # click_on "See Jobs"
-    # click_on "New Job"
-    # fill_in "Job title", with: "Test Job"
-    # fill_in "Location", with: "location"
-    # fill_in "Category", with: "category"
-    # click_button "Submit"
-    
-    # visit job_path
-    # click on "Test Job"
+  end
   
-    # assert_text "Forbidden"
-    # assert_equal page.current_path, login_path
-   
+  test "user can create a review" do
+    login_user
+    job = FactoryBot.create :job
+    visit root_path
+    click_on "See Jobs"
+    click_on job.job_title
+    click_on "Reviews about this job"
+    click_on "Add Review"
+    fill_in "Work hours", with: "Test hours"
+    fill_in "Pros", with: "Test pros"
+    fill_in "Cons", with: "Test cons"
+    click_button "Submit Review"
+    
+    assert_text "Test pros"
+  end
+  
+  test "user can edit a review they created" do
+    login_user
+    job = FactoryBot.create :job
+    visit root_path
+    click_on "See Jobs"
+    click_on job.job_title
+    click_on "Reviews about this job"
+    click_on "Add Review"
+    fill_in "Work hours", with: "Test hours"
+    fill_in "Pros", with: "Test pros"
+    fill_in "Cons", with: "Test cons"
+    click_button "Submit Review"
+    
+    assert_text "Test pros"
   end
 end
