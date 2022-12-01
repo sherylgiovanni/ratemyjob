a15829b6a5a03cd416acbbeaf0966a08fdf3e808
Ubuntu <ubuntu@ip-172-31-19-155.us-west-2.compute.internal>
2022-12-01 08:09:58 +0000

    Add model unit test for review

--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -1,7 +1,36 @@
 require "test_helper"
 
 class ReviewTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "has a valid factory" do
+    review = FactoryBot.build :review
+    assert review.valid?
+  end
+  
+  test "requires a user" do
+    review = FactoryBot.build :review
+    review.user = nil
+    
+    refute review.valid?
+  end
+  
+  test "requires pros" do
+    review = FactoryBot.build :review
+    review.pros = ""
+    
+    refute review.valid?
+  end
+  
+  test "requires cons" do
+    review = FactoryBot.build :review
+    review.cons = ""
+    
+    refute review.valid?
+  end
+  
+  test "requires work hours" do
+    review = FactoryBot.build :review
+    review.work_hours = ""
+    
+    refute review.valid?
+  end
 end
